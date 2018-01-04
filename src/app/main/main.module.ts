b485bcfdcda1006f7db51dd179bb0fa7b68b2c78
Maaser <wen19950525@126.com>
2018-01-04 15:44:41 +0800

    init guard

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MainRoutes } from './main.routes';
+import { HasLoginGuard } from '../guard/has-login.guard';
 
 import { SidebarComponent } from './common/sidebar/sidebar.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
@@ -12,6 +13,9 @@ import { MainComponent } from './main.component';
     CommonModule,
     RouterModule.forChild(MainRoutes)
   ],
-  declarations: [SidebarComponent, NavbarComponent, MainComponent]
+  declarations: [SidebarComponent, NavbarComponent, MainComponent],
+  providers: [
+    HasLoginGuard
+  ]
 })
 export class MainModule { }
--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './main.component';
+import { HasLoginGuard } from '../guard/has-login.guard';
 
 export const MainRoutes: Routes = [
   {
     path: '',
-    component: MainComponent
+    component: MainComponent,
+    canActivate: [HasLoginGuard]
   }
 ];
