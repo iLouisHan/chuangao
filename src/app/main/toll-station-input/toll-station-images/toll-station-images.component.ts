453333cd663c4e70fe8c12ebf1912687dce2f9ce
Maaser <wen19950525@126.com>
2018-01-14 17:28:31 +0800

    add toll-station-input images

--- src/app/main/toll-station-input/toll-station-images/toll-station-images.component.ts
+++ src/app/main/toll-station-input/toll-station-images/toll-station-images.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-toll-station-images',
@@ -6,10 +10,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toll-station-images.component.scss']
 })
 export class TollStationImagesComponent implements OnInit {
+  login: Observable<any>;
+  imgArr: Array<any> = [];
+  orgCode: string;
 
-  constructor() { }
+  constructor(
+    private http: Http,
+    private store: Store<any>
+  ) {
+    this.login = store.select('login');
+  }
+
+  getImages(orgCode) {
+    this.http.get(`http://119.29.144.125:8080/cgfeesys/BaseInfo/getOrgPicPath?orgCode=${orgCode}`)
+            .map(res => res.json())
+            .subscribe(res => {
+              if (res.code) {
+                this.imgArr = res.data;
+              }
+            });
+  }
+
+  upload($event) {
+    const formdata = new FormData();
+    formdata.append('file', $event.target.files[0]);
+    formdata.append('orgCode', this.orgCode);
+    this.http.post(`http://119.29.144.125:8080/cgfeesys/upload/baseInfo`, formdata)
+      .map(res => res.json())
+      .subscribe(res => {
+        if (res.code) {
+          alert(res.message);
+          this.getImages(this.orgCode);
+        }else {
+          alert(res.message);
+        }
+      });
+  }
+
+  delete(id) {
+    this.http.get(`http://119.29.144.125:8080/cgfeesys/BaseInfo/deleteOrgPic?fileId=${id}`)
+            .map(res => res.json())
+            .subscribe(res => {
+              if (res.code) {
+                alert(res.message);
+                this.getImages(this.orgCode);
+              } else {
+                alert(res.message);
+              }
+            });
+  }
 
   ngOnInit() {
+    this.login.subscribe(res => {
+      if (res && res.orgType === 1) {
+        this.orgCode = res.orgCode;
+        this.getImages(res.orgCode);
+      }
+    });
   }
 
 }
